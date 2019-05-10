0eb1f8b8e77dccb109cfd8e5d4285de77c91380d
David Durschlag <ddurschlag@6river.com>
2019-05-10 17:17:44 -0400

    fix: cleanups

--- src/primitiveGuards.ts
+++ src/primitiveGuards.ts
@@ -1,6 +1,6 @@
 import {checkerToGuard} from './Checker';
-import {thenGuard} from './combinators';
 import {ReasonGuard} from './ReasonGuard';
+import {orGuard} from './combinators';
 
 type Primitive = 'string'|'number'|'bigint'|'boolean'|'symbol'|'undefined'|'object'|'function';
 // Dangerous -- do not export!
@@ -16,15 +16,25 @@ export const isNumber = getPrimitiveTypeCheck<number>('number');
 export const isString = getPrimitiveTypeCheck<string>('string');
 export const isBoolean = getPrimitiveTypeCheck<boolean>('boolean');
 export const isFunction = getPrimitiveTypeCheck<Function>('function');
-export const isSymbol = getPrimitiveTypeCheck<Symbol>('symbol');
+export const isSymbol = getPrimitiveTypeCheck<symbol>('symbol');
 export const isBigInt = getPrimitiveTypeCheck<BigInt>('bigint');
 
-type StringValuesOf<T> = T extends string ? T : never;
-type ArrayToLiteral<T, K extends keyof T = keyof T> = {[P in K]: StringValuesOf<T[P]>}[K];
-type Boolsy<T extends string> = {[P in T]: boolean};
-type LiteralCheck<T1 extends string, T2 extends string, BAD = unknown> =
-	Boolsy<T1> extends Boolsy<T2> ? (Boolsy<T2> extends Boolsy<T1> ? T1 : BAD) : BAD;
-export type ArrayLiteralCheck<T extends string, TT> = LiteralCheck<T, ArrayToLiteral<TT>>;
+type Literable = string | symbol | number;
+
+type ArrayToLiteral<T> = T extends ReadonlyArray<infer U> ? U : never;
+type BoolMap<T extends Literable> = {[P in T]: boolean};
+type LiteralCheck<T1 extends Literable, T2 extends Literable, BAD = unknown> =
+	BoolMap<T1> extends BoolMap<T2> ? (BoolMap<T2> extends BoolMap<T1> ? T1 : BAD) : BAD;
+export type ArrayLiteralCheck<T extends Literable, TT extends ReadonlyArray<Literable>> =
+	LiteralCheck<T, ArrayToLiteral<TT>>;
+
+export const isLiterable = orGuard<unknown, Literable>(
+	orGuard<unknown, string|symbol>(
+		isString,
+		isSymbol
+	),
+	isNumber
+);
 
 /**
  * Check that a value is a string literal type given the list of values.
@@ -32,21 +42,27 @@ export type ArrayLiteralCheck<T extends string, TT> = LiteralCheck<T, ArrayToLit
  *
  * @param keys Values to check
  */
-export function isStringLiteral<T extends string, U extends T>(
-	keys: ReadonlyArray<U>
+export function isLiteral<T extends Literable, U extends T>(
+	keys: ReadonlyArray<U>,
+	literableGuard: ReasonGuard<unknown, Literable> = isLiterable
 ): ReasonGuard<unknown, ArrayLiteralCheck<T, typeof keys>> {
 	// want this to be computed once when building the guard
-	const values = new Set<string>(keys);
-	const litGuard = checkerToGuard((x: string): string => {
-		if (values.has(x)) {
-			return `is ${x}`;
-		} else {
-			throw new Error(`not in ${keys}`);
+	const values = new Set<Literable>(keys);
+
+	return (input, es, cs): input is ArrayLiteralCheck<T, typeof keys> => {
+		try {
+			if (!literableGuard(input, es, cs)) {
+				return false;
+			}
+			if (values.has(input)) {
+				cs.push(`is ${String(input)}`);
+				return true;
+			} else {
+				throw new Error(`not in ${keys}`);
+			}
+		} catch (err) {
+			es.push(err);
+			return false;
 		}
-	});
-	// have to `unknown` the middle type and `as any` the literal guard here
-	// because `ArrayLiteralCheck<T, typeof keys>` might evaluate to `never`
-	// if it does, the caller is going to end up with a compiler error
-	// and so the fact that the typing here went weird should be moot
-	return thenGuard<unknown, unknown, ArrayLiteralCheck<T, typeof keys>>(isString, litGuard as any);
+	};
 };
