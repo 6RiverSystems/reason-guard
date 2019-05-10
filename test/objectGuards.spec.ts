abc6f9451180844c67a0b4e4e1836bb3ccb793f8
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-05-09 23:18:47 -0400

    fix: more examples of typescript breakage

--- test/objectGuards.spec.ts
+++ test/objectGuards.spec.ts
@@ -1,5 +1,6 @@
 import { PropertyGuards, objectHasDefinition } from '../src/objectGuards';
-import { isBoolean } from '../src/primitiveGuards';
+import { isBoolean, isString } from '../src/primitiveGuards';
+import { hasBooleanProperty, ReasonGuard } from '../src';
 
 type _From = {
 	a: string;
@@ -12,14 +13,35 @@ type _To = {
 	c: boolean;
 };
 
-type _GT = PropertyGuards<_From, _To>;
-
 // TODO: why does this give an error if you explicitly type it as PropertyGuards<_From, _To>?
 // it works in typescript 3.3.4000
 // it fails in 3.4.1 (next release)
 // I think the
-const _G: _GT = {
+const _G: PropertyGuards<_From, _To> = {
 	c: isBoolean,
 };
 
 const _g = objectHasDefinition(_G);
+
+type _FromComplex = {
+	a: string;
+	b: {
+		c: number;
+	};
+};
+type _ToComplex = {
+	a: string;
+	b: {
+		c: number;
+		d: boolean;
+	};
+	e: string;
+};
+const _GC: PropertyGuards<_FromComplex, _ToComplex> = {
+	// TODO: without the cast this doesn't work, it's not smart enough to infer the from[b] instead of unknown
+	// TODO: even with the cast, typescript >= 3.4.1 fails to recognize that 'b' is in the list of different properties
+	b: hasBooleanProperty('d') as ReasonGuard<_FromComplex['b'], _ToComplex['b']>,
+	e: isString,
+}
+
+const _gc = objectHasDefinition(_GC);
