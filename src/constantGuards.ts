d8c047f41c2c48e0cf86226e93116373c46c1916
David Durschlag <ddurschlag@6river.com>
2019-05-14 18:39:15 -0400

    fix: simplify constant guards

--- src/constantGuards.ts
+++ src/constantGuards.ts
@@ -1,17 +1,13 @@
 import {ReasonGuard} from './ReasonGuard';
+import {checkerToGuard} from './Checker';
 
-const trueGuard: ReasonGuard<unknown, unknown> = (input, _errs, confs = []): input is unknown => {
-	confs.push('true');
-	return true;
-};
-const falseGuard: ReasonGuard<unknown, never> = (input, errs = []): input is never => {
-	try {
+const trueGuard: ReasonGuard<unknown, unknown> =
+	checkerToGuard(() => 'true');
+
+const falseGuard: ReasonGuard<unknown, never> =
+	checkerToGuard(() => {
 		throw new Error('false');
-	} catch (err) {
-		errs.push(err);
-		return false;
-	}
-};
+	});
 
 export const constantGuards: (result: boolean) => ReasonGuard<unknown, unknown> =
 	(result: boolean) => result ? trueGuard : falseGuard;
