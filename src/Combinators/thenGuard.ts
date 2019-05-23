d4556ed927375368a49a5b329110a99639654f88
David Durschlag <ddurschlag@6river.com>
2019-05-23 13:45:06 -0400

    WIP: trying to get 'then' type correct

--- src/Combinators/thenGuard.ts
+++ src/Combinators/thenGuard.ts
@@ -3,27 +3,27 @@ import {orGuard} from './orGuard';
 import {notGuard} from './notGuard';
 import {buildNegatable} from '../NegatableGuard';
 
-export const thenGuard = <FROM, MID extends FROM, TO extends MID>(
+export const thenGuard = <FROM, MID extends FROM, TO extends FROM>(
 	left: ReasonGuard<FROM, MID>,
-	right: ReasonGuard<MID, TO>
+	right: ReasonGuard<MID&FROM, MID&TO>
 ) => buildNegatable(
-		() => getRawThen(left, right),
-		() => getRawNegatedThen(left, right)
+		() => getRawThen<FROM, MID, TO>(left, right),
+		() => getRawNegatedThen<FROM, MID, TO>(left, right)
 	);
 export const andGuard = thenGuard;
 
-function getRawThen<FROM, MID extends FROM, TO extends MID>(
+function getRawThen<FROM, MID extends FROM, TO extends FROM>(
 	left: ReasonGuard<FROM, MID>,
-	right: ReasonGuard<MID, TO>
+	right: ReasonGuard<MID&FROM, MID&TO>
 ): ReasonGuard<FROM, MID&TO> {
-	return (input, output, confirmations): input is TO => {
+	return (input, output, confirmations): input is MID&TO => {
 		return left(input, output, confirmations) && right(input, output, confirmations);
 	};
 }
 
-function getRawNegatedThen<FROM, MID extends FROM, TO extends MID>(
+function getRawNegatedThen<FROM, MID extends FROM, TO extends FROM>(
 	left: ReasonGuard<FROM, MID>,
-	right: ReasonGuard<MID, TO>
+	right: ReasonGuard<MID&FROM, MID&TO>
 ): ReasonGuard<FROM, FROM> {
 	return orGuard(
 		getRawThen(left, notGuard<MID, MID>(right)),
--- test/Combinators/thenGuard.spec.ts
+++ test/Combinators/thenGuard.spec.ts
@@ -1,6 +1,9 @@
 import {testTwoArgCombinator} from './testCombinator';
-import {thenGuard} from '../../src';
+import {thenGuard, ReasonGuard} from '../../src';
 
 describe('then/and', function() {
 	testTwoArgCombinator(',', [false, false, false, true], thenGuard);
 });
+
+const x: ReasonGuard<unknown, unknown> = undefined as any;
+const y = thenGuard(x, x);
