ac2ff3ddf7fcc4120def22fdd3ed463ea6a36019
David Durschlag <ddurschlag@6river.com>
2019-06-03 12:31:57 -0400

    fix: simplify types and code

--- src/objectGuards.ts
+++ src/objectGuards.ts
@@ -15,51 +15,28 @@ type NarrowedFields<FROM, TO extends FROM, K extends keyof FROM = keyof FROM> = 
  */
 type ExtendedFields<FROM, TO extends FROM> = Exclude<keyof TO, keyof FROM>;
 
-export type NarrowedGuards<FROM extends object, TO extends FROM> = {
-	[P in NarrowedFields<FROM, TO>]: (p: P) => ReasonGuard<Pick<FROM, P>, Pick<TO, P>>
-}
-
-export type ExtendedGuards<FROM, TO extends FROM> = {
-	[P in ExtendedFields<FROM, TO>]: (p: P) => ReasonGuard<unknown, Pick<TO, P>>;
-}
-
-export type PropertyGuards<FROM extends object, TO extends FROM> = NarrowedGuards<FROM, TO> & ExtendedGuards<FROM, TO>;
-
 /**
  * Fields in `TO` that are different (type or presence) in `FROM`
  */
-export type ChangedFields<FROM extends object, TO extends FROM> = keyof PropertyGuards<FROM, TO>;
+export type ChangedFields<FROM extends object, TO extends FROM> = NarrowedFields<FROM, TO>|ExtendedFields<FROM, TO>;
+
+type Beta<FROM extends object, TO extends FROM, P extends ChangedFields<FROM, TO>> =
+	(p: P) => ReasonGuard<Pick<FROM, P&keyof FROM>, Pick<TO, P>>;
+export type PropertyGuards<FROM extends object, TO extends FROM> = {
+	[P in ChangedFields<FROM, TO>]: Beta<FROM, TO, P>;
+}
 
 function checkDefinition<FROM extends object, TO extends FROM>(
 	definition: PropertyGuards<FROM, TO>, input: FROM, output: Error[], confirmations: string[],
 ): input is TO {
 	let anyPassed = false;
 	let anyFailed = false;
-	const narrowHalf: NarrowedGuards<FROM, TO> = definition;
-	const extendHalf: ExtendedGuards<FROM, TO> = definition;
-
-	function checkNarrow<K extends NarrowedFields<FROM, TO>>(k: K) {
-		if (narrowHalf[k](k)(input, output, confirmations)) {
-			anyPassed = true;
-		} else {
-			anyFailed = true;
-		}
-	}
-	function checkExtend<K extends ExtendedFields<FROM, TO>>(k: K) {
-		if (extendHalf[k](k)(input, output, confirmations)) {
-			anyPassed = true;
-		} else {
-			anyFailed = true;
-		}
-	}
 
 	function checkProperty<K extends ChangedFields<FROM, TO>>(k: K) {
-		if (k in input) {
-			const fromKey: NarrowedFields<FROM, TO> = k as any; // Can't prove this to TS :(
-			checkNarrow(fromKey);
+		if (definition[k](k)(input, output, confirmations)) {
+			anyPassed = true;
 		} else {
-			const fromKey: ExtendedFields<FROM, TO> = k as any; // Can't prove this to TS :(
-			checkExtend(fromKey);
+			anyFailed = true;
 		}
 	}
 
