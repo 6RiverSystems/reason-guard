5bbb2eac016485477a5144f44e990b1d842b5b05
matgr1 <matgr1@gmail.com>
2019-06-06 17:13:45 -0400

    feat: add arrayFilterGuard

---
+++ test/arrayFilterGuard.spec.ts
@@ -0,0 +1,14 @@
+import {arrayFilterGuard, isString} from '../src';
+import {assert} from 'chai';
+
+describe('arrayFilterGuard', function() {
+	const guard = arrayFilterGuard(isString);
+	it('works as a filter function and does not mutate input', function() {
+		const junk = ['foo', 'bar', 1, 2];
+		const junkClone = [...junk];
+
+		const strings = junk.filter(guard);
+		assert.deepEqual(strings, ['foo', 'bar']);
+		assert.deepEqual(junk, junkClone);
+	});
+});
