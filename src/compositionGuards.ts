b9df0e4c9731b2bd688996a208406ff1318fb5f8
Jamal Dawson <jdawson@6river.com>
2019-06-28 15:15:06 -0400

    fix: added date or stringy date guard

---
+++ src/compositionGuards.ts
@@ -0,0 +1,12 @@
+import {isString} from './primitiveGuards';
+import {isDate} from './instanceGuards';
+import {isDateString} from './parseGuards';
+import {thenGuard, orGuard} from './Combinators';
+
+export const isDateOrStringyDate = orGuard(
+	thenGuard(
+		isString,
+		isDateString
+	),
+	isDate
+);
