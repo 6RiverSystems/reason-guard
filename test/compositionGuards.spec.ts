96ab44ff9e31109e5babe2b0d3a0964ecb62cb4b
Jamal Dawson <jdawson@6river.com>
2019-06-28 15:35:55 -0400

    fix: add negative case

--- test/compositionGuards.spec.ts
+++ test/compositionGuards.spec.ts
@@ -1,5 +1,6 @@
 import {assert} from 'chai';
-
+import * as Chance from 'chance';
+const chance = new Chance();
 import {isDateOrStringyDate} from '../src';
 
 describe('compositionGuards', function() {
@@ -10,7 +11,13 @@ describe('compositionGuards', function() {
 	it('should return true for a date', function() {
 		assert.isTrue(isDateOrStringyDate(date));
 	});
-	it('should return true for a stringyDate date', function() {
+	it('should return true for a stringy date', function() {
 		assert.isTrue(isDateOrStringyDate(date.toISOString()));
+	});
+	it('should return false for a non-date string', function() {
+		assert.isFalse(isDateOrStringyDate(chance.word()));
+	});
+	it('should return false for a number', function() {
+		assert.isFalse(isDateOrStringyDate(chance.integer()));
 	});
 });
