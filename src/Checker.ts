c365b89687db14cdacf2ea564b41e19f23849cc3
Jamal Dawson <jdawson@6river.com>
2019-07-09 12:20:42 -0400

    fix: first pass at isUUID

--- src/Checker.ts
+++ src/Checker.ts
@@ -1,14 +1,30 @@
 import {ReasonGuard} from './ReasonGuard';
 import {NegatableGuard, buildNegatable} from './NegatableGuard';
+import {thenGuard} from './Combinators';
+import {isString} from './primitiveGuards';
 
 export type Checker<FROM> = (input: FROM) => string;
 
 export const checkerToGuard: <FROM, TO extends FROM, N extends FROM = FROM>(
 	checker: Checker<FROM>
-) => NegatableGuard<FROM, TO, N> = (checker) => buildNegatable(
-	() => getRawGuard(checker),
-	() => getRawNegation(checker)
-);
+	) => NegatableGuard<FROM, TO, N> = (checker) => buildNegatable(
+		() => getRawGuard(checker),
+		() => getRawNegation(checker)
+	);
+
+const uuidChecker = (input: string) => {
+	if (!!input
+			&& /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/.test(input)
+	) {
+		return 'Valid UUID';
+	} else {
+		throw new Error('Not a valid UUID');
+	}
+};
+
+const isUUIDString = checkerToGuard(uuidChecker);
+
+export const isUUID = thenGuard(isString, isUUIDString);
 
 function getRawGuard<FROM, TO extends FROM>(checker: Checker<FROM>): ReasonGuard<FROM, TO> {
 	return (input, e = [], c = []): input is TO => {
--- test/Checker.spec.ts
+++ test/Checker.spec.ts
@@ -1,6 +1,6 @@
 import {assert} from 'chai';
 
-import {checkerToGuard} from '../src';
+import {checkerToGuard, isUUID} from '../src';
 
 describe('checkerToGuard', function() {
 	const trueChecker = () => 'true';
@@ -15,5 +15,15 @@ describe('checkerToGuard', function() {
 	it('should fill a default for the confirmations list', function() {
 		// mostly asserting this does not throw
 		assert.isFalse(checkerToGuard(falseChecker)({}, [], undefined));
+	});
+
+	context('isUUID', function() {
+		it('should guard for a valid UUID', function() {
+			assert.isTrue(isUUID('0ca8f69c-1d07-4404-9b82-d1d0eb492313'));
+		});
+
+		it('should guard against a invalid UUID', function() {
+			assert.isFalse(isUUID(3));
+		});
 	});
 });
